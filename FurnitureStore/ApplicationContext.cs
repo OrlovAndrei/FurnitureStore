41b264bad12629954147fbeb1bf62fa075c090d3
Andrey <androrvit@gmail.com>
2023-02-01 14:13:21 +0300

    add ApplicationContext

---
+++ FurnitureStore/ApplicationContext.cs
@@ -0,0 +1,22 @@
+﻿using FurnitureStore.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FurnitureStore
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Product> Products => Set<Product>();
+        public DbSet<ProductCategory> ProductCategories => Set<ProductCategory>();
+        public DbSet<Order> Orders => Set<Order>();
+        
+        public ApplicationContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.Use
+        //}
+    }
+}
