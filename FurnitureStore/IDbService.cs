7cc2a0910f878c75f5db81fe8c7280840139d8d2
Andrey <androrvit@gmail.com>
2023-02-01 21:10:28 +0300

    add IDbService

---
+++ FurnitureStore/IDbService.cs
@@ -0,0 +1,15 @@
+﻿using FurnitureStore.Models;
+
+namespace FurnitureStore
+{
+    public interface IDbService
+    {
+        List<Product> GetAllProductsByCategoryId(int id);
+
+        Product GetProductById(int id);
+
+        List<Category> GetAllCategory();
+
+        void AddOrder(Order order);
+    }
+}
