f067983deaeaa4674b4b13ffd92d524ca9e6ee57
Andrey <androrvit@gmail.com>
2023-02-01 14:00:00 +0300

    add models

---
+++ FurnitureStore/Models/Order.cs
@@ -0,0 +1,12 @@
+﻿namespace FurnitureStore.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public Product Product { get; set; }
+        public decimal OrderPrice { get; set; }
+        public int Number { get; set; }
+        public DateTime Time { get; set; }
+        public string Adress { get; set; }
+    }
+}
--- FurnitureStore/Models/Product.cs
+++ FurnitureStore/Models/Product.cs
@@ -1,6 +1,13 @@
-﻿namespace FurnitureStore.Models
+﻿using System.ComponentModel;
+
+namespace FurnitureStore.Models
 {
     public class Product
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Discription { get; set; }
+        public decimal Price { get; set; }
+        public ProductCategory Category { get; set; }
     }
 }
